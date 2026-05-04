9cfa1b74ff700a61c5b98a44b36a9e216093d375
Naveen Saini <naveensaini.ddun@gmail.com>
2026-05-04 13:52:38 +0530

    initial commit

---
+++ src/main/java/com/legacyfirst/modagent/ModagentApplication.java
@@ -0,0 +1,13 @@
+package com.legacyfirst.modagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModagentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/legacyfirst/modagent/ModagentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legacyfirst.modagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModagentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
